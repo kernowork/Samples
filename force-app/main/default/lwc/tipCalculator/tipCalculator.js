ce7c5837d570731af601bc6e488772f832a22f58
Kernowork <14323418+kernowork@users.noreply.github.com>
2020-09-20 09:15:24 -0700

    TIp Calculator

---
+++ force-app/main/default/lwc/tipCalculator/tipCalculator.js
@@ -0,0 +1,43 @@
+import { LightningElement, track} from 'lwc';
+
+export default class TipCalculator extends LightningElement {
+    tipRate = 0.0;
+    customValue = false;
+    amount = 0.0;
+    tipAmount = 0.0;
+    totalAmount = 0.0;
+    numParty = 1;
+    eachAmount = 0.0;
+
+    handleAmount(event) {
+        this.amount = event.target.value;
+        this.computeAmounts();
+    }
+
+    handleNumParty(event) {
+        this.numParty = event.target.value;
+        this.computeAmounts();
+    }
+
+    handleClick(event) {
+        this.customValue = false;
+        this.tipRate= event.target.value;
+        this.computeAmounts();
+    }
+
+    handleCustomClick(event) {
+        this.customValue = true;
+    }
+
+    handleCustomTip(event) {
+        this.tipRate = event.target.value;
+        this.computeAmounts();
+    }
+
+    computeAmounts() {
+        //this.amount is actually a string type!
+        this.tipAmount = this.amount * this.tipRate;
+        this.totalAmount = this.amount * 1.0 + this.tipAmount;
+        this.eachAmount = this.totalAmount / this.numParty;
+    }
+}
